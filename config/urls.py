a25b46d55f6fb6019be6fa88093f9c8531a45a15
go-arnold <arnoldciku@gmail.com>
2026-04-22 16:50:33 +0000

    Code quality Checks

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,11 @@ from shortener.views import RedirectShortURLView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("api/", include("api.urls")),
     path("<str:short_code>/", RedirectShortURLView.as_view(), name="redirect-short-url"),
 ]
--- shortener/migrations/0001_initial.py
+++ shortener/migrations/0001_initial.py
@@ -7,17 +7,24 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='ShortURL',
+            name="ShortURL",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('original_url', models.URLField(max_length=2048)),
-                ('short_code', models.CharField(max_length=6, unique=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("original_url", models.URLField(max_length=2048)),
+                ("short_code", models.CharField(max_length=6, unique=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
